ad53a48908a3b98b57bc4219731ff13496279963
Leonardo Henrique <88909238+leonardohenriquedev@users.noreply.github.com>
2021-12-08 19:14:27 -0300

    Delete saveCartItems.js

--- saveCartItems.js
+++
@@ -1,7 +0,0 @@
-const saveCartItems = (cartItems = document.querySelector('.cart__items').innerHTML) => {
-  localStorage.setItem('cartItems', cartItems);
-};
-
-if (typeof module !== 'undefined') {
-  module.exports = saveCartItems;
-}
